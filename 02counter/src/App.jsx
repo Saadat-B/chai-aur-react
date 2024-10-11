be6bbe5cee86465320232234dd4177ab412910ab
Saadat-B <saadatbadgujar@gmail.com>
2024-10-11 12:07:53 +0530

    add 02counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [counter, setCounter] = useState(15);
+
+  const addValue = () => {
+    console.log({ counter });
+    counter = counter + 1;
+    setCounter(counter);
+  };
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={addValue}>Add value</button>
+      <br />
+      <hr />
+      <button>remove value</button>
+    </>
+  );
+}
+
+export default App;
